f7df58fbe5546870881464c8e9cb6fb1c2cbaaf5
Ryley Johnson <ryleyjohnsonemail@gmail.com>
2024-09-10 00:03:53 -0700

    test suite begins with filling addresses

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -3,6 +3,12 @@ const helper = require('../../helper')
 
 describe('Create an order', () => {
 
+    it('should fill in the addresses for route', async () => {
+        await browser.url(`/`)
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+
+    }),
+
     it('should open phone number modal', async () => {
         await browser.url(`/`)
         await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
